2bb9dc07a2a70a80f0e835e6a6269deee3cae1a4
Pranjal kr Gautam <pranjalgautam180@gmail.com>
2026-08-30 16:16:35 +0530

    Time: 4 ms (44.37%) | Memory: 52.1 MB (74.20%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -10,7 +10,7 @@ class Solution {
             if(nums[right] == 0){
                 zeroes++;
             }
-            while(zeroes > k){
+            if(zeroes > k){
                 if(nums[left] == 0){
                     zeroes--;
                 }
